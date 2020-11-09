2110976e05e4b8965c980c73e6213324abe8774f
buecherwurmmm <buecherwurm@immerda.ch>
2020-11-09 17:45:09 +0100

    fix

---
+++ ProjectUtumno_full.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="ProjectUtumno_full" tilewidth="32" tileheight="32" tilecount="6080" columns="64">
+ <image source="ProjectUtumno_full.png" width="2048" height="3040"/>
+</tileset>
